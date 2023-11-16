f25067143385086a3d28e3bd4f2b0144803abae8
Sebastian F. Sanchez <sfsanchez@stro.unam.mx>
2023-11-16 09:16:49 -0800

    Only one core used now

--- lvmdap/_cmdline/dap.py
+++ lvmdap/_cmdline/dap.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python3
 
 import sys, os
+os.environ["OMP_NUM_THREADS"] = "1" # export OMP_NUM_THREADS=1
+os.environ["OPENBLAS_NUM_THREADS"] = "1" # export OPENBLAS_NUM_THREADS=1
+os.environ["MKL_NUM_THREADS"] = "1" # export MKL_NUM_THREADS=1
+os.environ["VECLIB_MAXIMUM_THREADS"] = "1" # export VECLIB_MAXIMUM_THREADS=1
+os.environ["NUMEXPR_NUM_THREADS"] = "1" # export NUMEXPR_NUM_THREADS=1
+
+
+
 import time
 from astropy.io.fits.column import _parse_tdim
 import numpy as np
