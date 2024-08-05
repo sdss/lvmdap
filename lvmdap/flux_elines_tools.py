5a1413ae5c0f0ae9cc84e4eac4709da6e2261c10
sfsanchez72 <sfsanchez@astrol.unam.mx>
2024-08-05 04:43:25 -0700

    Non-parametric updated 2

--- lvmdap/flux_elines_tools.py
+++ lvmdap/flux_elines_tools.py
@@ -130,7 +130,7 @@ def flux_elines_RSS_EW(flux__wyx, input_header, n_MC, elines_list, vel__yx, sigm
                          flux__wyx[i, :][mask_wave],color='black')
                 plt.plot([w_m_guess,w_m_guess],[np.min(flux__wyx[i, :][mask_wave]),np.max(flux__wyx[i, :][mask_wave])],color='orange')
                 plt.show(block=True)
-                print('# :',vel_I1, 1.217*(I2/2.354)**2, EW, s_I0, s_vel_I1, s_I2, e_EW)
+            print('# :',vel_I1, 1.217*(I2/2.354)**2, EW, s_I0, s_vel_I1, s_I2, e_EW)
 
 #                time.sleep(5)
 
