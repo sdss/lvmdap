1183863b285d9fd29b82d5d89643cc8c0eea729a
Sebastian F. Sanchez <sfsanchez@stro.unam.mx>
2025-05-01 14:44:26 +0100

    New notebook created to generate the model from the fitting

--- lvmdap/_cmdline/gen_output_model.py
+++ lvmdap/_cmdline/gen_output_model.py
@@ -60,6 +60,14 @@ from lvmdap.dap_tools import list_columns,read_DAP_file,map_plot_DAP
 from lvmdap.dap_tools import fit_legendre_polynomial
 from lvmdap.dap_tools import find_redshift_spec, replace_nan_inf_with_adjacent_avg
 
+
+from lvmdap._cmdline.dap import auto_rsp_elines_rnd
+from lvmdap.pyFIT3D.common.stats import calc_chi_sq, smooth_ratio, shift_convolve, hyperbolic_fit_par
+from lvmdap.pyFIT3D.common.constants import __c__, __shift_convolve_lnwl_frac__, __sigma_to_FWHM__
+from lvmdap.pyFIT3D.modelling.dust import spec_apply_dust
+from lvmdap.pyFIT3D.common.io import array_to_fits, trim_waves, sel_waves, print_verbose, write_img_header
+
+
 #
 # Just for tests
 #
@@ -85,154 +93,6 @@ def _no_traceback(type, value, traceback):
   print(value)
 
 
-
-#######################################################
-# RSP version of the auto_ssp_elines_rnd from lvmdap.pyFIT3D
-#######################################################
-def auto_rsp_elines_rnd(
-    wl__w, f__w, ef__w, ssp_file, spaxel_id, config_file=None, plot=None,
-    ssp_nl_fit_file=None, sigma_inst=None, mask_list=None,
-    min=None, max=None, w_min=None, w_max=None,
-    nl_w_min=None, nl_w_max=None, elines_mask_file=None, fit_gas=True, refine_gas=True,
-    input_redshift=None, delta_redshift=None, min_redshift=None, max_redshift=None,
-    input_sigma=None, delta_sigma=None, min_sigma=None, max_sigma=None, sigma_gas=None,
-    input_AV=None, delta_AV=None, min_AV=None, max_AV=None, ratio=True, y_ratio=None,
-    fit_sigma_rnd=True, out_path=None, SPS_master=None, SN_CUT=2):
-
-  #
-  # If there is no RSP for the Non-Linear (nl) fitting, they it is 
-  # used the one for the Linear Fitting (that it is slower)
-  #
-    ssp_nl_fit_file = ssp_file if ssp_nl_fit_file is None else ssp_nl_fit_file
-    if delta_redshift == 0:
-      cc_redshift_boundaries = None
-    else:
-      cc_redshift_boundaries = [min_redshift, max_redshift]
-
-  #
-  # If the emission lines are fitted, but there is no config file, then
-  # the program creates a set of configuraton files for the detected emission lines
-  # NOTE: I think this is overdoing having the flux_elines script
-  # But needs to be explored!
-  #
-    if fit_gas and config_file is None:
-        print("##############################");
-        print("# START: Autodectecting emission lines...");
-        if sigma_gas is None: sigma_gas = 3.0
-        if out_path is None: out_path = "."
-        wl_mask = (w_min<=wl__w)&(wl__w<=w_max)
-        config_filenames, wl_chunks, _, wave_peaks_tot_rf = detect_create_ConfigEmissionModel(
-            wl__w[wl_mask], f__w[wl_mask],
-            redshift=input_redshift,
-            sigma_guess=sigma_gas,
-            chunks=4,
-            polynomial_order=1,
-            polynomial_coeff_guess=[0.000, 0.001],
-            polynomial_coeff_boundaries=[[-1e13, 1e13], [-1e13, 1e13]],
-            flux_boundaries_fact=[0.001, 1000],
-            sigma_boundaries_fact=[0.1, 1.5],
-            v0_boundaries_add=[-1000, 1000],
-            peak_find_nsearch=1,
-            peak_find_threshold=0.2,
-            peak_find_dmin=1,
-            crossmatch_list_filename=elines_mask_file,
-            crossmatch_absdmax_AA=5,
-            crossmatch_redshift_search_boundaries=cc_redshift_boundaries,
-            sort_by_flux=True,
-            output_path=out_path,
-            label=spaxel_id,
-            verbose=0,
-            plot=0,
-        )
-
-        create_emission_lines_mask_file_from_list(wave_peaks_tot_rf, eline_half_range=3*sigma_gas, output_path=out_path, label=spaxel_id)
-        create_emission_lines_file_from_list(wave_peaks_tot_rf, output_path=out_path, label=spaxel_id)
-        create_ConfigAutoSSP_from_lists(wl_chunks, config_filenames, output_path=out_path, label=spaxel_id)
-
-        config_file = os.path.join(out_path, f"{spaxel_id}.autodetect.auto_ssp_several.config")
-        if not refine_gas: elines_mask_file = os.path.join(out_path, f"{spaxel_id}.autodetect.emission_lines.txt")
-        print("# END: Autodectecting emission lines...");     
-    else:
-      print("# Using predefined configuration file for the emission lines");
-    #
-    # The spectrum is fitted for the 1st time in here
-    #
-    print("##############################");
-    print(f"# START: fitting the continuum+emission lines, fit_gas:{fit_gas} ...");
-    cf, SPS = auto_rsp_elines_single_main(
-        wl__w, f__w, ef__w, ssp_file,
-        config_file=config_file,
-        ssp_nl_fit_file=ssp_nl_fit_file, sigma_inst=sigma_inst, out_file="NOT_USED",
-        mask_list=mask_list, elines_mask_file=elines_mask_file, fit_gas=fit_gas,
-        min=min, max=max, w_min=w_min, w_max=w_max, nl_w_min=nl_w_min, nl_w_max=nl_w_max,
-        input_redshift=input_redshift, delta_redshift=delta_redshift,
-        min_redshift=min_redshift, max_redshift=max_redshift,
-        input_sigma=input_sigma, delta_sigma=delta_sigma, min_sigma=min_sigma, max_sigma=max_sigma,
-        input_AV=input_AV, delta_AV=delta_AV, min_AV=min_AV, max_AV=max_AV,
-        plot=plot, single_ssp=False, ratio=ratio, y_ratio=y_ratio, fit_sigma_rnd=fit_sigma_rnd,
-        sps_class=StellarSynthesis, SPS_master=SPS_master , SN_CUT=  SN_CUT 
-    )
-    print(f"# END: fitting the continuum+emission lines, fit_gas:{fit_gas} ...");
-    print("##############################");
-    #
-    # There is refinement in the fitting
-    #
-    print(f"# refine_gas: {refine_gas}");
-    if refine_gas:
-        print(f"# START: refining gas fitting, refine_gas:{refine_gas} ...");
-        if sigma_gas is None: sigma_gas = 3.0
-        if out_path is None: out_path = "."
-        wl_mask = (w_min<=wl__w)&(wl__w<=w_max)
-        gas_wl, gas_fl = SPS.spectra["orig_wave"][wl_mask], (SPS.output_spectra_list[0] - SPS.output_spectra_list[1])[wl_mask]
-        config_filenames, wl_chunks, _, wave_peaks_tot_rf = detect_create_ConfigEmissionModel(
-            gas_wl, gas_fl,
-            redshift=input_redshift,
-            sigma_guess=sigma_gas,
-            chunks=4,
-            polynomial_order=1,
-            polynomial_coeff_guess=[0.000, 0.001],
-            polynomial_coeff_boundaries=[[-1e13, 1e13], [-1e13, 1e13]],
-            flux_boundaries_fact=[0.001, 1000],
-            sigma_boundaries_fact=[0.1, 1.5],
-            v0_boundaries_add=[-1000, 1000],
-            peak_find_nsearch=1,
-            peak_find_threshold=0.2,
-            peak_find_dmin=1,
-            crossmatch_list_filename=elines_mask_file,
-            crossmatch_absdmax_AA=5,
-            crossmatch_redshift_search_boundaries=cc_redshift_boundaries,
-            sort_by_flux=True,
-            output_path=out_path,
-            label=spaxel_id,
-            verbose=0,
-            plot=0,
-        )
-
-        create_emission_lines_mask_file_from_list(wave_peaks_tot_rf, eline_half_range=3*sigma_gas, output_path=out_path, label=spaxel_id)
-        create_emission_lines_file_from_list(wave_peaks_tot_rf, output_path=out_path, label=spaxel_id)
-        create_ConfigAutoSSP_from_lists(wl_chunks, config_filenames, output_path=out_path, label=spaxel_id)
-
-        config_file = os.path.join(out_path, f"{spaxel_id}.autodetect.auto_ssp_several.config")
-        elines_mask_file = os.path.join(out_path, f"{spaxel_id}.autodetect.emission_lines.txt")
-
-        cf, SPS = auto_rsp_elines_single_main(
-            wl__w, f__w, ef__w, ssp_file,
-            config_file=config_file,
-            ssp_nl_fit_file=ssp_nl_fit_file, sigma_inst=sigma_inst, out_file="NOT_USED",
-            mask_list=mask_list, elines_mask_file=elines_mask_file, fit_gas=fit_gas,
-            min=min, max=max, w_min=w_min, w_max=w_max, nl_w_min=nl_w_min, nl_w_max=nl_w_max,
-            input_redshift=input_redshift, delta_redshift=delta_redshift,
-            min_redshift=min_redshift, max_redshift=max_redshift,
-            input_sigma=input_sigma, delta_sigma=delta_sigma, min_sigma=min_sigma, max_sigma=max_sigma,
-            input_AV=input_AV, delta_AV=delta_AV, min_AV=min_AV, max_AV=max_AV,
-            plot=plot, single_ssp=False, ratio=ratio, y_ratio=y_ratio, fit_sigma_rnd=fit_sigma_rnd,
-            sps_class=StellarSynthesis
-        )
-        print(f"# END: refining gas fitting, refine_gas:{refine_gas} ...");
-        print("########################################");
-    print("# END RSP fitting...");
-    print("########################################");
-    return cf, SPS
 
 
 
@@ -348,7 +208,11 @@ def _main(cmd_args=sys.argv[1:]):
   except:
     n_leg = 11
 
-  
+  try:
+    ny_range=args.ny_range
+  except:
+    ny_range=None
+        
   lvmCFrame=args.lvm_file
   dap_file=args.DAP_table_in
   out_file_fit = os.path.join(args.output_path, f"{args.label}.output.fits.gz")
@@ -372,9 +236,19 @@ def _main(cmd_args=sys.argv[1:]):
   tab_PT_org = read_PT(lvmCFrame,'none',ny_range=None)
 
 
-  hdu_org['FLUX'].data=hdu_org['FLUX'].data[tab_PT_org['mask']]
-
+  if (ny_range is not None) and (ny_range[0] is not None):    
+    tab_PT_org_range = tab_PT_org[ny_range[0]:ny_range[1]]
+    print(len(tab_PT_org_range))
+    hdu_org['FLUX'].data=hdu_org['FLUX'].data[ny_range[0]:ny_range[1],:]
+    hdu_org['FLUX'].data=hdu_org['FLUX'].data[tab_PT_org_range['mask']]
+  else:
+    hdu_org['FLUX'].data=hdu_org['FLUX'].data[tab_PT_org['mask']]
   hdu_org['FLUX'].data = replace_nan_inf_with_adjacent_avg( hdu_org['FLUX'].data)
+
+  
+#  hdu_org['FLUX'].data=hdu_org['FLUX'].data[tab_PT_org['mask']]
+
+#  hdu_org['FLUX'].data = replace_nan_inf_with_adjacent_avg( hdu_org['FLUX'].data)
 
   
   
@@ -464,7 +338,7 @@ def _main(cmd_args=sys.argv[1:]):
   a_redshift=np.array(tab_DAP['redshift_st'])
   a_sigma=np.array(tab_DAP['disp_st'])
   a_AV=np.array(tab_DAP['Av_st'])
-
+  a_flux_st=np.array(tab_DAP['med_flux_st'])
   n_coeffs=0
   for cols in tab_DAP.columns:
         if (cols.find('min_W_rsp_')>-1):
@@ -475,7 +349,15 @@ def _main(cmd_args=sys.argv[1:]):
     w_id = f'W_rsp_{i}'
     a_coeffs_input[:,i]=np.array(tab_DAP[w_id])
   
-  
+
+  ###################################################################################
+  # We read the RSP file
+  ###################################################################################
+  hdu_rsp = fits.open(args.rsp_file)
+  rsp_data = hdu_rsp['SPECTRA'].data
+  rsp_wave = hdu_rsp['SPECTRA'].header['CRVAL1']+ hdu_rsp['SPECTRA'].header['CDELT1']* (np.arange(0, hdu_rsp['SPECTRA'].header['NAXIS1']))
+    #hdu_rsp.info()
+    
   for i,model_st in enumerate(model_spectra[1,:,:]):
    # print(f'### {i}/{ny}')
    # print(f'### (AV,redshift,sigma) : {a_AV[i]},{a_redshift[i]},{a_sigma[i]}')
@@ -484,51 +366,77 @@ def _main(cmd_args=sys.argv[1:]):
     res_spec = 0.1*np.abs(org_spec)
     wl__w, f__w, ef__w = wave, org_spec, res_spec
     out_file = os.path.join(args.output_path, f"junk_{args.label}.fits")
-
+    model_rsp = np.nansum(rsp_data.T*coeffs_input,axis=1)
+    model_rsp = shift_convolve(wave, rsp_wave, model_rsp, a_redshift[i], a_sigma[i], float(args.sigma_inst))
+    model_spectra[1,i,:] = spec_apply_dust(wave, model_rsp*a_flux_st[i]/f_scale, a_AV[i], R_V=3.1, extlaw='CCM')
     
-    if (i==0):
-      SPS_master=None
-
-    _, SPS = model_rsp_elines_single_main(
-      wl__w, f__w, ef__w, args.rsp_file,  config_file, out_file, ssp_nl_fit_file=args.rsp_file,
-      w_min=args.w_range[0], w_max=args.w_range[1], nl_w_min=args.w_range_nl[0],
-      nl_w_max=args.w_range_nl[1], mask_list=args.mask_file,
-      min=args.flux_scale[0], max=args.flux_scale[1], elines_mask_file=args.emission_lines_file,
-      fit_gas=not args.ignore_gas, 
-      input_redshift=a_redshift[i], delta_redshift=0, 
-      min_redshift=a_redshift[i], max_redshift=a_redshift[i],
-      input_sigma=a_sigma[i], delta_sigma=0, min_sigma=a_sigma[i], max_sigma=a_sigma[i],
-      input_AV=a_AV[i], delta_AV=0, min_AV=a_sigma[i], max_AV=a_sigma[i],
-      sigma_inst=args.sigma_inst, plot=args.plot, sps_class=StellarSynthesis,
-      coeffs_input=coeffs_input, SPS_master=SPS_master
-    )
-    SPS_master=SPS
+   # if (i==0):
+   #   SPS_master=None
+   #
+   #_, SPS = model_rsp_elines_single_main(
+   #  wl__w, f__w, ef__w, args.rsp_file,  config_file, out_file, ssp_nl_fit_file=args.rsp_file,
+   #   w_min=args.w_range[0], w_max=args.w_range[1], nl_w_min=args.w_range_nl[0],
+    #  nl_w_max=args.w_range_nl[1], mask_list=args.mask_file,
+    #  min=args.flux_scale[0], max=args.flux_scale[1], elines_mask_file=args.emission_lines_file,
+    #  fit_gas=not args.ignore_gas, 
+    #  input_redshift=a_redshift[i], delta_redshift=0, 
+    #  min_redshift=a_redshift[i], max_redshift=a_redshift[i],
+    #  input_sigma=a_sigma[i], delta_sigma=0, min_sigma=a_sigma[i], max_sigma=a_sigma[i],
+    #  input_AV=a_AV[i], delta_AV=0, min_AV=a_sigma[i], max_AV=a_sigma[i],
+     # sigma_inst=args.sigma_inst, plot=args.plot, sps_class=StellarSynthesis,
+     # coeffs_input=coeffs_input, SPS_master=SPS_master
+    #)
+    #SPS_master=SPS
     #print(i)
     #print(SPS.spectra['model_min'].shape)
     #print(SPS.spectra['model_min'])
-    model_st = np.array(SPS.spectra['model_min'])
-    model_spectra[1,i,:] = model_st
+   # model_st = np.array(SPS.spectra['model_min'])
+   # model_spectra[1,i,:] = model_st
 #    print(model_st)
-    print(f'model_st stats: {np.median(model_st)} {np.min(model_st)} {np.max(model_st)}')
+    #print(f'model_st stats: {np.median(model_st)} {np.min(model_st)} {np.max(model_st)}')
     #print(model_st.shape)
     #model_spectra[1,i,:]=SPS.spectra['model_min']
     print(f'### **** {i}/{ny} *****')
 
-#  print(model_spectra.shape)
-  gas_spectra = model_spectra[0,:,:] - model_spectra[1,:,:]
-  l_smooth_spectra = gas_spectra*0.0
-  for idx,gas in enumerate(gas_spectra): 
-    smooth = median_filter(gas, size=smooth_size, mode='reflect')
-    l_smooth = fit_legendre_polynomial(wl__w, smooth,n_leg)
-    # print(f'{idx} {np.median(smooth)} {np.median(l_smooth)}')
-    l_smooth_spectra[idx,:] = l_smooth
+
+  res_spectra = model_spectra[0,:,:] - model_spectra[1,:,:] - model_spectra[6,:,:]
+  l_smooth_spectra = res_spectra*0.0
+  print(smooth_size,n_leg)
+  for idx,res in enumerate(res_spectra): 
+    smooth = median_filter(res, size=smooth_size)
+    l_smooth = fit_legendre_polynomial(wave, smooth, n_leg)
+    l_smooth_spectra[idx] = l_smooth
   model_spectra[2,:,:] = model_spectra[1,:,:] + model_spectra[6,:,:] + l_smooth_spectra
   model_spectra[3,:,:] = model_spectra[0,:,:] - (model_spectra[1,:,:] + l_smooth_spectra)
   model_spectra[4,:,:] = model_spectra[0,:,:] - model_spectra[2,:,:]
   model_spectra[5,:,:] = model_spectra[0,:,:] - model_spectra[6,:,:]
+  model_spectra[8,:,:] = model_spectra[1,:,:] + model_spectra[7,:,:] + l_smooth_spectra
 
-  dump_rss_output(out_file_fit=out_file_fit, wavelength=wl__w, model_spectra=model_spectra)
-#  hdu_org.writeto(out_file_fit,overwrite=True)
+
+  ##################################################################################
+  # Writting the result
+  ##################################################################################
+  #dump_rss_output(out_file_fit=out_file_fit, wavelength=wl__w, model_spectra=model_spectra)
+
+  fits_name = out_file_fit if out_file_fit.endswith(".gz") else out_file_fit+".gz"
+  # hdr = fits.Header()
+  h = {}
+  h['CRPIX1'] = 1
+  h['CRVAL1'] = wave[0]
+  h['CDELT1'] = wave[1] - wave[0]
+  h['NAME0'] = 'org_spec'
+  h['NAME1'] = 'model_spec'
+  h['NAME2'] = 'mod_joint_spec'
+  h['NAME3'] = 'gas_spec'
+  h['NAME4'] = 'res_joint_spec'
+  h['NAME5'] = 'no_gas_spec'
+  h['NAME6'] = 'gas_model_NP'
+  h['NAME7'] = 'gas_model_PEK'
+  h['NAME8'] = 'mod_joint_spec_PEK'
+  h["FILENAME"] = fits_name
+  array_to_fits(fits_name, model_spectra, overwrite=True)
+  write_img_header(fits_name, list(h.keys()), list(h.values()))
+  #  hdu_org.writeto(out_file_fit,overwrite=True)
   #sys.stdout = original_stdout
   print("#############################")
   print('# ALL DONE !!! ')
